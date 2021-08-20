571594c484618a24edee3f8a9c9b979d1c88a067
Sourabi Kannan <sourabikannan@gmail.com>
2021-08-20 18:51:37 +0530

    db models,api,auth

---
+++ models.py
@@ -0,0 +1,86 @@
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+
+
+DATABASE_PATH = os.environ.get('DATABASE_URL')
+db = SQLAlchemy()
+
+
+def setup_db(app, db_path=DATABASE_PATH):
+    """
+    Initializes database
+    """
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+def update():
+    db.session.commit()
+
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.DateTime())
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
+
+
+class Actors(db.Model):
+    __tablename__ = 'actors'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(10), nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+export DATABASE_URL="postgresql://postgres:3008@localhost:5432/capstone"
+export AUTH0_DOMAIN="fsnano.us.auth0.com"
+export ALGORITHMS=["RS256"]
+export API_AUDIENCE="castingagency"
